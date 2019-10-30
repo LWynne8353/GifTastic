4c53083100b8735e2db95181289e04dc07eb456b
liamwynne <liamdwynne5753@gmail.com>
2019-10-30 09:47:34 -0400

    fill in code

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//$(document).ready(function){
+
+//});
+
+
+//
+//var query https://api.giphy.com/v1/gifs/search?api_key=8zRpsfahc5Fqb30C0sk05hJ2YhNm5AZU&q=muppets&limit=10&offset=0&rating=PG&lang=en
+
+//$.ajax({
+ //   url: queryURL,
+//    method: "GET"
+//  }).then(function(response) {
+//    console.log(response);
+//  });
