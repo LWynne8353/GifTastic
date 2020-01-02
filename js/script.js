dcb1a4ad4a17a47db677797397f5da65460401b7
liamwynne <liamdwynne5753@gmail.com>
2020-01-02 09:47:13 -0500

    got the api to work, now time to break down and append

--- js/script.js
+++ js/script.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
     // Grabbing and storing the data-muppet property value from the button
     muppets = $(this).attr("data-muppets");
 
-    var queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + muppets + "api_key=8zRpsfahc5Fqb30C0sk05hJ2YhNm5AZU&&q=randomize and shuffle react&limit=10&offset=&rating=PG-13&lang=en");
+    //var queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + muppets + "api_key=8zRpsfahc5Fqb30C0sk05hJ2YhNm5AZU&limit=10&offset=&rating=PG-13&lang=en");
 
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8zRpsfahc5Fqb30C0sk05hJ2YhNm5AZU&q=" + muppets +"&limit=10&offset=&rating=PG-13&lang=en";
     $.ajax({
       url: queryURL,
       method: "GET"
