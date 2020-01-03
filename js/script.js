4ec8b21c592d8205432f67b08d0eca837dfcdd7e
liamwynne <liamdwynne5753@gmail.com>
2020-01-02 19:23:11 -0500

    ten muppet buttons work

--- js/script.js
+++ js/script.js
@@ -1,18 +1,31 @@
 $(document).ready(function () {
 
   $("button").on("click", function () {
-    // Grabbing and storing the data-muppet property value from the button
+    // Calling on what muppet gifs to get
     muppets = $(this).attr("data-muppets");
-
-    //var queryURL = ("https://api.giphy.com/v1/gifs/search?q=" + muppets + "api_key=8zRpsfahc5Fqb30C0sk05hJ2YhNm5AZU&limit=10&offset=&rating=PG-13&lang=en");
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8zRpsfahc5Fqb30C0sk05hJ2YhNm5AZU&q=" + muppets +"&limit=10&offset=&rating=PG-13&lang=en";
+    //queryURL to gather the gifs
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=8zRpsfahc5Fqb30C0sk05hJ2YhNm5AZU&q=" + muppets + "&limit=10";
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function (response) {
       console.log(queryURL);
       console.log(response);
+
+      var muppet = response.data;
+
+      for (var i = 0; i < muppet.length; i++) {
+        if (muppet[i].rating !== "r" && muppet[i].rating !== "pg-13") {
+          var imageDiv = $("<div>");
+          var p = $("<p>").text("Rating: " + muppet[i].rating);
+          var muppetGif = $("<img>");
+          muppetGif.attr("src", muppet[i].images.fixed_height.url);
+
+          imageDiv.append(p);
+          imageDiv.append(muppetGif);
+          $("#muppetsgiphys").prepend(imageDiv);
+        }
+      }
     });
   })
 
